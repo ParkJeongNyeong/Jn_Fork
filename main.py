1a58147db1cddf7a5c2a9ffac22677dad323508a
Hwang-jun-woo <miend1234@gmail.com>
2024-12-06 14:23:28 +0900

    hcommit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import max_profit as mp
 import prime_number as pn
 
-if __name__ == '__main__':
-    pn.is_prime_number(1)
-    pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(97)
-    pn.is_prime_number(37)
+if __name__ != '__main__':
+    pn.is_prime_number(188)
+    pn.is_prime_number(138)
+    pn.is_prime_number(248)
+    pn.is_prime_number(978)
+    pn.is_prime_number(378)
