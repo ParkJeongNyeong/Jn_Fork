f6097c31172a8d331b9bb8e48d4c70b89c9c22ad
mooncat01 <lrel1823@gmail.com>
2024-12-06 14:29:41 +0900

    modify main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import max_profit as mp
 import prime_number as pn
 
+    pn.is_prinme_number(12)
 if __name__ == '__main__':
-    pn.is_prime_number(1)
-    pn.is_prime_number(13)
+    pn.is_prime_number(2)
     pn.is_prime_number(24)
     pn.is_prime_number(97)
-    pn.is_prime_number(334)
     pn.is_prime_number(22)
